695352ea79de3364138212233efd5c9f0883fd97
amilkar checa <cheko_999_@hotmail.com>
2019-04-05 00:52:08 -0400

    preg 1

---
+++ solutions/addTwoDigits.js
@@ -0,0 +1,16 @@
+//1. Dado un número de dos dígitos devolver la suma de esos dos dígitos.
+//Ejemplo. para n = 29, ​addTwoDigits(n) = 11
+
+
+function addTwoDigits(n) {
+
+    var aux1 = n%10;
+    var aux2 = parseInt(n/10);
+    var suma = aux1+aux2;
+
+  return suma;
+}
+
+console.log(addTwoDigits(29));
+//console.log(digitos(32));
+//console.log(digitos(55));
